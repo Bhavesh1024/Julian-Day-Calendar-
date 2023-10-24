2f6d7883c23ddf42fabf30603d576e8e5d27c73f
roas23 <143058305+roas23@users.noreply.github.com>
2023-10-24 17:33:43 +0000

    My First Commit of Parking Function

--- part-2/parking_functions.cc
+++ part-2/parking_functions.cc
@@ -1,35 +1,52 @@
-// TODO: Add the required header
+// Saraii Roa
+// roas23@csu.fullerton.edu
+// @roas23
+// Partners: @bhavesh1024
 
 #include "parking_functions.h"
 
 bool CanParkOnAsh(const std::string& day, int hour) {
-  // TODO: Write an if statement that returns true when parking is allowed on
+  // xTODO: Write an if statement that returns true when parking is allowed on
   // Ash Street, or false otherwise.
   // HINT: The if statement needs a compound Boolean expression with multiple
   // && operators.
-  return false;
+  if (day == "wed" && hour >= 10 && hour <= 12) {
+    return false;
+  return true;
 }
 
 bool CanParkOnBeech(const std::string& day, int hour) {
-  // TODO: Write an if statement that returns true when parking is allowed on
-  // Beech Street, or false otherwise.
-  return false;
+  if (hour >= 8 && hour <= 12) {
+    return false;
+  }
+  return true;
 }
-
 bool CanParkOnCedar(const std::string& day, int hour) {
-  // TODO: Write an if statement that returns true when parking is allowed on
+  // xTODO: Write an if statement that returns true when parking is allowed on
   // Cedar Street, or false otherwise.
-  return false;
-}
+  if (hour >= 700 && hour <= 1900) {
+    return false;
+  } if (day == "tue" && hour >= 8 && hour <= 10) {
+    return false;
+  }
+  return true;
+  }
 
 bool CanParkOnDate(const std::string& day, int hour, int min) {
-  // TODO: Write an if statement that returns true when parking is allowed on
-  // Date Street, or false otherwise.
-  return false;
-}
-
+  if ((day == "sat" || day == "sun" && (hour <= 6  && min <= 30) || ( hour >= 16))) {
+    return false;
+  }
+   return true;
+  }
 bool CanParkOnElm(const std::string& day, int hour) {
-  // TODO: Write an if statement that returns true when parking is allowed on
+  // xTODO: Write an if statement that returns true when parking is allowed on
   // Elm Street, or false otherwise.
-  return false;
-}
+  if ((day == "mon" && day == "wed" && day == "thu") && hour >= 8 && hour <= 20) {
+    return false;
+  } if (day == "fri" && hour >= 8 && hour <= 17) {
+    return false;
+  } if (day == "tue") {
+    return false;
+  }
+  return true;
+  } 
