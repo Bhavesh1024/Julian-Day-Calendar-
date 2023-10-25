50f40f70115f77e353ac1b3ba63fb3af324ee420
Bhavesh1024 <143297804+Bhavesh1024@users.noreply.github.com>
2023-10-25 04:00:04 +0000

    Made changes to run parking functions

--- part-2/parking_functions.cc
+++ part-2/parking_functions.cc
@@ -6,47 +6,46 @@
 #include "parking_functions.h"
 
 bool CanParkOnAsh(const std::string& day, int hour) {
-  // xTODO: Write an if statement that returns true when parking is allowed on
-  // Ash Street, or false otherwise.
-  // HINT: The if statement needs a compound Boolean expression with multiple
-  // && operators.
   if (day == "wed" && hour >= 10 && hour <= 12) {
     return false;
-  return true;
+  } else {
+    return true;
+  }
 }
 
 bool CanParkOnBeech(const std::string& day, int hour) {
-  if (hour >= 8 && hour <= 12) {
+  if (day == "fri" && hour >= 8 && hour <= 12) {
     return false;
+  } else {
+    return true;
   }
-  return true;
 }
 bool CanParkOnCedar(const std::string& day, int hour) {
-  // xTODO: Write an if statement that returns true when parking is allowed on
-  // Cedar Street, or false otherwise.
-  if (hour >= 700 && hour <= 1900) {
+  if (hour > 19 || hour <= 7) {
     return false;
-  } if (day == "tue" && hour >= 8 && hour <= 10) {
+  } else if (day == "tue" && (hour >= 8 && hour <= 10)) {
     return false;
+  } else {
+    return true;
   }
-  return true;
-  }
+}
 
 bool CanParkOnDate(const std::string& day, int hour, int min) {
-  if ((day == "sat" || day == "sun" && (hour <= 6  && min <= 30) || ( hour >= 16))) {
+  if ((day == "sat" || day == "sun")) {
+    return true;
+  } else if ((hour > 6 || (hour == 6 && min >= 30)) && (hour <= 16)) {
     return false;
+  } else {
+    return true;
   }
-   return true;
-  }
+}
 bool CanParkOnElm(const std::string& day, int hour) {
-  // xTODO: Write an if statement that returns true when parking is allowed on
-  // Elm Street, or false otherwise.
-  if ((day == "mon" && day == "wed" && day == "thu") && hour >= 8 && hour <= 20) {
+  if ((day == "mon" || day == "wed" || day == "thu") &&
+      (hour >= 8 && hour <= 20)) {
     return false;
-  } if (day == "fri" && hour >= 8 && hour <= 17) {
+  } else if (day == "fri" && hour >= 8 && hour <= 17) {
     return false;
-  } if (day == "tue") {
-    return false;
+  } else {
+    return true;
   }
-  return true;
-  } 
+}
